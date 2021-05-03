fda9cd4ef2bd64deaa6a522d2f8d8163dfb87e9f
Leonardo Omaña <leotangram@gmail.com>
2021-05-03 15:57:40 -0500

    Validate input forms

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,5 +1,20 @@
-const Form = () => {
-  return <h1>Create product</h1>
-}
+import {InputLabel, Select, TextField} from '@material-ui/core'
+
+const Form = () => (
+  <>
+    <h1>Create product</h1>
+    <form>
+      <TextField label="name" id="name" />
+      <TextField label="size" id="size" />
+      <InputLabel htmlFor="type">Type</InputLabel>
+      <Select native value="" inputProps={{name: 'type', id: 'type'}}>
+        <option aria-label="None" value="None" />
+        <option value="electronic">Electronic</option>
+        <option value="furniture">Furniture</option>
+        <option value="clothing">Clothing</option>
+      </Select>
+    </form>
+  </>
+)
 
 export default Form
