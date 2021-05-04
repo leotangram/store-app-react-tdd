df5571fda0ec9cd3b988bc89f0376d354278acfd
Leonardo Omaña <jose.omana@rappi.com>
2021-05-04 14:43:17 -0500

    Validation test inputs with submit

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,21 +1,55 @@
-import {Button, InputLabel, Select, TextField} from '@material-ui/core'
+import { useState } from 'react'
+import { Button, InputLabel, Select, TextField } from '@material-ui/core'
 
-const Form = () => (
-  <>
-    <h1>Create product</h1>
-    <form>
-      <TextField label="name" id="name" />
-      <TextField label="size" id="size" />
-      <InputLabel htmlFor="type">Type</InputLabel>
-      <Select native value="" inputProps={{name: 'type', id: 'type'}}>
-        <option aria-label="None" value="None" />
-        <option value="electronic">Electronic</option>
-        <option value="furniture">Furniture</option>
-        <option value="clothing">Clothing</option>
-      </Select>
-      <Button>Submit</Button>
-    </form>
-  </>
-)
+const Form = () => {
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    size: '',
+    type: '',
+  })
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const { name, size, type } = e.target.elements
+
+    if (!name.value) {
+      setFormErrors(prevState => ({
+        ...prevState,
+        name: 'The name is required',
+      }))
+    }
+    if (!size.value) {
+      setFormErrors(prevState => ({
+        ...prevState,
+        size: 'The size is required',
+      }))
+    }
+    if (!type.value) {
+      setFormErrors(prevState => ({
+        ...prevState,
+        type: 'The type is required',
+      }))
+    }
+  }
+
+  return (
+    <>
+      <h1>Create product</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField label="name" id="name" helperText={formErrors.name} />
+        <TextField label="size" id="size" helperText={formErrors.size} />
+        <InputLabel htmlFor="type">Type</InputLabel>
+        <Select native value="" inputProps={{ name: 'type', id: 'type' }}>
+          <option aria-label="None" value="" />
+          <option value="electronic">Electronic</option>
+          <option value="furniture">Furniture</option>
+          <option value="clothing">Clothing</option>
+        </Select>
+        {formErrors.type.length && <p>{formErrors.type}</p>}
+        <Button type="submit">Submit</Button>
+      </form>
+    </>
+  )
+}
 
 export default Form
--- src/form/Form.test.js
+++ src/form/Form.test.js
@@ -1,25 +1,48 @@
-import {screen, render} from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
 import Form from './Form'
 
-describe('when the form is mounted', () => {
+describe('<Form />', () => {
   beforeEach(() => render(<Form />))
+  describe('when the form is mounted', () => {
+    it('should there must be a create product form page', () => {
+      expect(
+        screen.getByRole('heading', { name: /create product/i }),
+      ).toBeInTheDocument()
+    })
 
-  it('should there must be a create product form page', () => {
-    expect(
-      screen.getByRole('heading', {name: /create product/i}),
-    ).toBeInTheDocument()
+    it('should exists the fields: name, size, type (electronic, furniture, clothing)', () => {
+      expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
+      expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
+      expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
+      expect(screen.queryByText(/electronic/i)).toBeInTheDocument()
+      expect(screen.queryByText(/furniture/i)).toBeInTheDocument()
+      expect(screen.queryByText(/clothing/i)).toBeInTheDocument()
+    })
+
+    it('should exists the submit button', () => {
+      expect(
+        screen.getByRole('button', { name: /submit/i }),
+      ).toBeInTheDocument()
+    })
   })
 
-  it('should exists the fields: name, size, type (electronic, furniture, clothing)', () => {
-    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
-    expect(screen.queryByText(/electronic/i)).toBeInTheDocument()
-    expect(screen.queryByText(/furniture/i)).toBeInTheDocument()
-    expect(screen.queryByText(/clothing/i)).toBeInTheDocument()
-  })
+  describe('when the user submits the form without values', () => {
+    it('should display validation messages', () => {
+      expect(
+        screen.queryByText(/the name is required/i),
+      ).not.toBeInTheDocument()
+      expect(
+        screen.queryByText(/the size is required/i),
+      ).not.toBeInTheDocument()
+      expect(
+        screen.queryByText(/the type is required/i),
+      ).not.toBeInTheDocument()
 
-  it('should exists the submit button', () => {
-    expect(screen.getByRole('button', {name: /submit/i})).toBeInTheDocument()
+      fireEvent.click(screen.getByRole('button', { name: /submit/i }))
+
+      expect(screen.queryByText(/the name is required/i)).toBeInTheDocument()
+      expect(screen.queryByText(/the size is required/i)).toBeInTheDocument()
+      expect(screen.queryByText(/the type is required/i)).toBeInTheDocument()
+    })
   })
 })
